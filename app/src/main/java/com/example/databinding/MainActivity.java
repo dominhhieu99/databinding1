553f17cfce4ccd46a3a7199f5724af99687bec59
Dobatrang03031966 <dohieu19999@gmail.com>
2019-07-16 13:06:05 +0700

    Initial commit

---
+++ app/src/main/java/com/example/databinding/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.databinding;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.databinding.databinding.ActivityMainBinding;
+import com.example.databinding.model.Wellcome;
+
+public class MainActivity extends AppCompatActivity {
+    private Wellcome welcome;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_welcome);
+
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        welcome = new Wellcome("Let's get started", "Never a better time than now to start thinking about how are manage all your finances whithease", "Creat Acount", "Log in");
+        binding.setWelcome(welcome);
+    }
+
+    public void register(View view) {
+        Intent intent = new Intent(MainActivity.this, Register_Step1_Activity.class);
+        startActivity(intent);
+    }
+}
